e8d2b9cbba013dfd3270b5ccb44aff273cdd20f5
Nedz-space <0xnedz@gmail.com>
2025-03-07 10:26:00 +0100

    Modified admin.py

--- relationship_app/admin.py
+++ relationship_app/admin.py
@@ -2,21 +2,30 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, UserProfile
+from .models import Author, Library, Book
 
 class CustomUserAdmin(UserAdmin):
-    list_display = ('username', 'email', 'date_of_birth', 'is_staff')
+    """Customize the Django Admin for the Custom User Model."""
+
+    model = CustomUser
+    list_display = ("username", "email", "date_of_birth", "is_staff", "is_active")
     fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        ('Personal Info', {'fields': ('email', 'date_of_birth', 'profile_photo')}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        ('Important Dates', {'fields': ('last_login', 'date_joined')}),
+        (None, {"fields": ("username", "email", "password")}),
+        ("Personal Info", {"fields": ("date_of_birth", "profile_photo")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "groups", "user_permissions")}),
     )
     add_fieldsets = (
         (None, {
-            'classes': ('wide',),
-            'fields': ('username', 'email', 'password1', 'password2', 'date_of_birth', 'profile_photo'),
+            "classes": ("wide",),
+            "fields": ("username", "email", "password1", "password2", "date_of_birth", "profile_photo", "is_staff", "is_active"),
         }),
     )
+    search_fields = ("email", "username")
+    ordering = ("email",)
+
 
 admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(UserProfile)
+admin.site.register(Author)
+admin.site.register(Library)
+admin.site.register(Book)
