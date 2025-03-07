ad460bccc38c23ab7f409bab16624f694f3d72ce
Nedz-space <0xnedz@gmail.com>
2025-03-07 11:06:07 +0100

    updated bookshelf/admin.py and model.py

--- bookshelf/admin.py
+++ bookshelf/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from .models import Book
-
-# Register your models here.
+from .models import Book, Author, Library
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')
+    list_display = ('title', 'author', 'publication_year')  # Ensure publication_year exists
     list_filter = ('author', 'publication_year')
-    search_fields = ('title', 'author')
+    search_fields = ('title', 'author__name')  # Fix for ForeignKey
 
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
+admin.site.register(Library)
+
--- bookshelf/models.py
+++ bookshelf/models.py
@@ -1,7 +1,6 @@
 
 # Create your models here.
 
-
 from django.db import models
 
 class Author(models.Model):
@@ -20,7 +19,7 @@ class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     library = models.ForeignKey(Library, related_name='books', on_delete=models.CASCADE, null=True, blank=True)
-    publication_year = models.PositiveIntegerField(null=True, blank=True)
+    publication_year = models.PositiveIntegerField(null=True, blank=True)  # Add this field
 
     def __str__(self):
         return self.title
