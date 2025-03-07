337e494cbb9b600948aea3def0330322c940aba3
Nedz-space <0xnedz@gmail.com>
2025-03-07 11:14:47 +0100

    updated settings.py

--- LibraryProject/settings.py
+++ LibraryProject/settings.py
@@ -16,6 +16,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 AUTH_USER_MODEL = 'relationship_app.CustomUser'
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
 
 # Redirect after login/logout
 LOGIN_REDIRECT_URL = '/'
