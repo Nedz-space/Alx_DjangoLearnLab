76adf470ad62487a4b94894b854ab048dbbc3ae9
Nedz-space <0xnedz@gmail.com>
2025-03-07 11:31:00 +0100

    modified models.py

--- bookshelf/models.py
+++ bookshelf/models.py
@@ -2,6 +2,7 @@
 # Create your models here.
 
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
@@ -23,3 +24,10 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+    def __str__(self):
+        return self.username
