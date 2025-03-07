ec7fce6b1e1b2de404d11f3ab0d50e38ecc73212
Nedz-space <0xnedz@gmail.com>
2025-03-07 10:41:50 +0100

    updated admin.py and models.py

--- bookshelf/models.py
+++ bookshelf/models.py
@@ -1,13 +1,26 @@
-from django.db import models
 
 # Create your models here.
 
+
 from django.db import models
 
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
+class Author(models.Model):
+    name = models.CharField(max_length=100)
 
     def __str__(self):
-        return f"{self.title} by {self.author} ({self.publication_year})"
+        return self.name
+
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    library = models.ForeignKey(Library, related_name='books', on_delete=models.CASCADE, null=True, blank=True)
+    publication_year = models.PositiveIntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
