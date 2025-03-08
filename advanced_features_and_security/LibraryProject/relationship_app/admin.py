375c36e9ebb5243f207d7fb6b5ca913af297e311
Nedz-space <0xnedz@gmail.com>
2025-03-08 22:09:13 +0100

    updated admin.py

--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -4,8 +4,6 @@ from django.contrib.auth.admin import UserAdmin
 from .models import Author, Library, Book
 
 
-admin.site.register(CustomUser, CustomUserAdmin)
-admin.site.register(UserProfile)
 admin.site.register(Author)
 admin.site.register(Library)
 admin.site.register(Book)
