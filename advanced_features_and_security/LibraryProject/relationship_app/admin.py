7f62eaee85248166ead939c575c596a2789e2fbf
Nedz-space <0xnedz@gmail.com>
2025-03-08 22:05:55 +0100

    models.py update

--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -3,25 +3,6 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import Author, Library, Book
 
-class CustomUserAdmin(UserAdmin):
-    """Customize the Django Admin for the Custom User Model."""
-
-    model = CustomUser
-    list_display = ("username", "email", "date_of_birth", "is_staff", "is_active")
-    fieldsets = (
-        (None, {"fields": ("username", "email", "password")}),
-        ("Personal Info", {"fields": ("date_of_birth", "profile_photo")}),
-        ("Permissions", {"fields": ("is_staff", "is_active", "groups", "user_permissions")}),
-    )
-    add_fieldsets = (
-        (None, {
-            "classes": ("wide",),
-            "fields": ("username", "email", "password1", "password2", "date_of_birth", "profile_photo", "is_staff", "is_active"),
-        }),
-    )
-    search_fields = ("email", "username")
-    ordering = ("email",)
-
 
 admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(UserProfile)
