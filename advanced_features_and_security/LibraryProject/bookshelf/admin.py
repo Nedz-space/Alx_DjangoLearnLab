4e727e0dfcb7b79bf13a00de8843c81c2ed9e281
Nedz-space <0xnedz@gmail.com>
2025-03-08 21:14:17 +0100

    admin.py update

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,13 +1,35 @@
 #Admin.py
 
 from django.contrib import admin
-from .models import Book, Author, Library
+from django.contrib.auth.admin import UserAdmin
+from .models import Book, Author, Library, CustomUser
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')  # Ensure publication_year exists
     list_filter = ('author', 'publication_year')
     search_fields = ('title', 'author__name')  # Fix for ForeignKey
 
+class CustomUserAdmin(UserAdmin):
+    """Admin customization for CustomUser."""
+    model = CustomUser
+    list_display = ('username', 'email', 'date_of_birth', 'is_staff', 'is_superuser')
+    search_fields = ('username', 'email')
+    list_filter = ('is_staff', 'is_superuser', 'date_of_birth')
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Personal Info', {'fields': ('date_of_birth', 'profile_photo')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important Dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'date_of_birth', 'profile_photo', 'is_staff', 'is_superuser'),
+        }),
+    )
+    ordering = ('email',)
+
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Book, BookAdmin)
 admin.site.register(Author)
 admin.site.register(Library)
