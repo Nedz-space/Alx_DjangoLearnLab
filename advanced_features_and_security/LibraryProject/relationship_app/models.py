34f02f5b8e245fc2886e30aefb330190263f7df9
Nedz-space <0xnedz@gmail.com>
2025-03-08 21:36:25 +0100

    models.py update

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -32,36 +32,7 @@ class Book(models.Model):
             ("can_delete_book", "Can delete book"),
         ]
 
-class CustomUserManager(BaseUserManager):
-    def create_user(self, email, username, password=None, **extra_fields):
-        if not email:
-            raise ValueError('The Email field must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, username=username, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, username, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
-        return self.create_user(email, username, password, **extra_fields)
-
-class CustomUser(AbstractUser):
-    date_of_birth = models.DateField(_('Date of Birth'), null=True, blank=True)
-    profile_photo = models.ImageField(_('Profile Photo'), upload_to='profile_photos/', null=True, blank=True)
-
-    objects = CustomUserManager()
-
-    def __str__(self):
-        return self.username
-
+    
 class UserProfile(models.Model):
     ROLE_CHOICES = (
         ('Admin', 'Admin'),
