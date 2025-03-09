d50afa70aa714003416618444f3f89cc950f4c19
Nedz-space <0xnedz@gmail.com>
2025-03-09 10:33:36 +0100

    modified views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -4,6 +4,19 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required, login_required
 from django.http import HttpResponseForbidden
 from .models import Book
+from .forms import SearchForm
+from django.db.models import Q
+
+
+def book_list(request):
+    form = SearchForm(request.GET or None)
+    books = Book.objects.all()
+
+    if form.is_valid():
+        query = form.cleaned_data.get('query')
+        books = books.filter(Q(title__icontains=query) | Q(author__name__icontains=query))
+
+    return render(request, 'bookshelf/book_list.html', {'books': books, 'form': form})
 
 @login_required
 @permission_required('bookshelf.can_view', raise_exception=True)
@@ -38,3 +51,7 @@ def book_delete(request, book_id):
     book.delete()
     return redirect('book_list')
 
+def secure_view(request):
+    response = render(request, 'bookshelf/secure_page.html')
+    response['Content-Security-Policy'] = "default-src 'self'; script-src 'self'; style-src 'self'"
+    return response
