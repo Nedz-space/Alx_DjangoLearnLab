9b3e79b87a582be4d96d59d493060f41f3a55651
Nedz-space <0xnedz@gmail.com>
2025-03-09 06:31:04 +0100

    updated models.py and admin.py

--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -2,8 +2,29 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import Author, Library, Book
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
 
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'library', 'publication_year')
 
-admin.site.register(Author)
-admin.site.register(Library)
-admin.site.register(Book)
+def setup_groups():
+    """Create groups and assign permissions"""
+    editors, _ = Group.objects.get_or_create(name="Editors")
+    viewers, _ = Group.objects.get_or_create(name="Viewers")
+    admins, _ = Group.objects.get_or_create(name="Admins")
+
+    content_type = ContentType.objects.get_for_model(Book)
+
+    # Define permissions
+    can_view = Permission.objects.get(codename="can_view", content_type=content_type)
+    can_create = Permission.objects.get(codename="can_create", content_type=content_type)
+    can_edit = Permission.objects.get(codename="can_edit", content_type=content_type)
+    can_delete = Permission.objects.get(codename="can_delete", content_type=content_type)
+
+    # Assign permissions
+    viewers.permissions.add(can_view)
+    editors.permissions.add(can_view, can_create, can_edit)
+    admins.permissions.add(can_view, can_create, can_edit, can_delete)
+
