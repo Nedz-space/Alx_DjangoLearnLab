17855608b3cc0d1b89d7aa8345e51f8ed049d382
Nedz-space <0xnedz@gmail.com>
2025-03-09 08:44:55 +0100

    Updated models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -25,6 +25,24 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey('Author', on_delete=models.CASCADE)
+    library = models.ForeignKey('Library', related_name='books', on_delete=models.CASCADE, null=True, blank=True)
+    publication_year = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+            ("can_view", "Can view book"),
+        ]
 
 
 class CustomUserManager(BaseUserManager):
