23b29f5ced1722101d7ff6d00e82c3b2dfeaf5b5
Nedz-space <0xnedz@gmail.com>
2025-03-11 17:05:42 +0100

    Modified views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -4,6 +4,7 @@ from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()  # Get all book instances
@@ -12,3 +13,6 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # Only authenticated users can access
+    permission_classes = [IsAdminUser]  # Only admins can CRUD books
+
