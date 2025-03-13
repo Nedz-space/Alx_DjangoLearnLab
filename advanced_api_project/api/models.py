df9d3dfce17430a692be5bf0f78a3bf125424514
Nedz-space <0xnedz@gmail.com>
2025-03-13 11:36:10 +0100

    Commiting the creation of django projectt

--- advanced_api_project/api/models.py
+++ advanced_api_project/api/models.py
@@ -1,3 +1,25 @@
+# Create your models here.
+
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    """
+    Author model stores the details of each author.
+    """
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    """
+    Book model stores information about each book and links to an Author.
+    """
+    title = models.CharField(max_length=255)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
