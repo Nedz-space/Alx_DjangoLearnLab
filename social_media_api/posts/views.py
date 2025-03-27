afae2c8bf51c29c30a88a783d4a5c26f5dd83f28
Nedz-space <0xnedz@gmail.com>
2025-03-27 12:10:12 +0100

    Modifications and creation of posts app and necessary scripts

---
+++ social_media_api/posts/views.py
@@ -0,0 +1,30 @@
+
+# Create your views here.
+
+from rest_framework import viewsets, permissions
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+from rest_framework import filters
+
+class PostViewSet(viewsets.ModelViewSet):
+    """Viewset for managing posts."""
+    queryset = Post.objects.all().order_by('-created_at')
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """Viewset for managing comments."""
+    queryset = Comment.objects.all().order_by('-created_at')
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class PostViewSet(viewsets.ModelViewSet):
+    ...
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'content']
